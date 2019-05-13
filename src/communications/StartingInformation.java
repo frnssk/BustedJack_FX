12022e9e24a33803c1fd8de9169c9e4afbb8264f
Rasmus Oberg <rasmus.oberg@hotmail.com>
2019-05-13 12:06:32 +0200

    new class to update clients

---
+++ src/communications/StartingInformation.java
@@ -0,0 +1,14 @@
+package communications;
+
+public class StartingInformation {
+	private int numberOfMinutes;
+	private int numberOfRounds;
+	private int minimumBet;
+	
+	public StartingInformation(int minutes, int rounds, int miniBet) {
+		this.numberOfMinutes = minutes;
+		this.numberOfRounds = rounds;
+		this.minimumBet = miniBet;
+	}
+
+}
