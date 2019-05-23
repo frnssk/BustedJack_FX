375ed863d3334927baa63b6e133e602aeef9bfc5
Isak Eklund <isak.eklund@gmaol.com>
2019-05-23 21:16:09 +0200

    Game Over view added

---
+++ src/communications/GameOver.java
@@ -0,0 +1,12 @@
+package communications;
+
+import java.io.Serializable;
+
+public class GameOver implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5279465549360050603L;
+
+}
